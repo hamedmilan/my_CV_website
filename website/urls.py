d533d3667b879d5bd94f874cbac6b8c20b2b0119
hamedmilan <hamed.milanchian@gmail.com>
2024-04-24 20:01:25 +0300

    fixed templates: base and others

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,11 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
     path('', index_view, name = 'index'),
+    path('about', about_view, name = 'about'),
+    path('services', service_view, name = 'services'),
+    path('contact', contact_view, name = 'contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about-us.html')
+
+
+def service_view(request):
+    return render(request, 'website/services.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
