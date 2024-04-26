9d122097f6bc2c4ca7d349eee0d3858897aec5f8
hamedmilan <hamed.milanchian@gmail.com>
2024-04-26 12:36:41 +0300

    sending data via rendering added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, 'website/index.html')
+    context = {'phone':'(+358) 046 683 5578','email':'hamed.milanchian@gmail.com','city_country':'Tampere/Finland'}
+    return render(request, 'website/index.html', context)
 
 
 def languages_view(request):
@@ -9,15 +10,18 @@ def languages_view(request):
 
 
 def education_view(request):
-    return render(request, 'website/education.html')
+    context = {'BSc':'Mechanical Engineering','MSc':'Mechanical Engineering','PhD':'Electrical Engineering'}
+    return render(request, 'website/education.html',context)
 
 def skills_view(request):
-    return render(request, 'website/skills.html')
+    context = {'skill_1':'Mathematical Modeling','skill_2':'FEM','skill_3':'Mechanical Analysis',
+               'skill_4':'Programming','skill_5':'Numerical Modeling','skill_6':'High Temperature Superconductors'}
+    return render(request, 'website/skills.html', context)
 
 def projects_view(request):
     return render(request, 'website/projects.html')
 
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
-
+    context = {'address':'Orivedenkatu 5 E 265'}
+    return render(request, 'website/contact.html', context)
